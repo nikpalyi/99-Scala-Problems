283e117d60f683ccc1c5809f80de7d9f30267f7a
splitcomma <mozijegy@hotmail.com>
2022-05-09 21:17:28 +0100

    Working more on P01

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,8 +1,10 @@
+import scala.annotation.tailrec
 
 object P01 extends App{
 
   def lastBuiltin[A](ls: List[A]): A = ls.last
 
+  @tailrec
   def lastRecursive[A](ls: List[A]): A = ls match {
     case h :: Nil => h
     case _ :: tail => lastRecursive(tail)
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -1,8 +1,22 @@
-import P01.lastBuiltin
-import org.scalatest.Matchers.convertToAnyShouldWrapper
+import P01.{lastBuiltin, lastRecursive}
+import org.scalatest.Matchers._
+import scala.language.postfixOps
 
 class P01Spec extends UnitSpec {
   "lastBuiltin" should "return the last element of a list" in {
     lastBuiltin(List(2, 4, 6, 5, 4, 7, 6)) shouldBe 6
   }
+
+  "lastRecursive" should "return last e of list" in {
+    lastRecursive(List(7, 7)) shouldBe (7)
+  }
+
+  "lastRecursive" should "return last the e of list" in {
+    lastRecursive(List(7)) shouldBe (7)
+  }
+// TODO
+//  "lastRecursive" should "throw exception in case of empty list" in {
+//    lastRecursive(List()) shouldBe ()
+//    intercept[NoSuchElementException] {lastRecursive()}
+//    }
 }

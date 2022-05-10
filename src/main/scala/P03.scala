05ccb4fcd8f47eef51f0ebd40fa645a8ce35ab70
splitcomma <mozijegy@hotmail.com>
2022-05-10 09:00:41 +0100

    adding P03 and test

--- src/main/scala/P03.scala
+++ src/main/scala/P03.scala
@@ -1,6 +1,6 @@
 
 object P03 {
 
-  //
-
+  //Find the Kth element of a list.
+  def kthElementInList[A](i: Int, list: List[A]): A = list(i)
 }
--- src/test/scala/P03Spec.scala
+++ src/test/scala/P03Spec.scala
@@ -1,7 +1,11 @@
+import P03.kthElementInList
+import org.scalatest.Matchers.convertToAnyShouldWrapper
 
 class P03Spec extends UnitSpec {
 
-  // TODO
+  "kthElementInList" should "return theKth element from a list" in {
+    kthElementInList(4, List(8,88,77,99,9,90,67)) shouldBe 9
+  }
 
 }
 
